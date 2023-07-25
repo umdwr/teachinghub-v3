e5ffd7fad8dfb319b02b02c7bd879b57ec638075
Andrew Davis <adavis46@outlook.com>
2023-07-24 19:20:17 -0500

    update theme?

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,6 +29,7 @@ author = 'DWR'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
