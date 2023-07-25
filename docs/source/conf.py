747c63bb37c1a85e771ae133f325a9c9bb95de8b
Andrew Davis <adavis46@outlook.com>
2023-07-24 19:45:45 -0500

    fix conf file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,51 +1,30 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+# For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# import sphinx_rtd_theme
-# sys.path.insert(0, os.path.abspath('.'))
-
-
 # -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'DWR Teaching Hub'
-copyright = '2021, Department of Writing and Rhetoric'
+project = 'myDWR Teaching Hub'
+copyright = '2023, DWR'
 author = 'DWR'
-
+release = '3'
 
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = [
-]
+extensions = []
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates',]
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
+templates_path = ['_templates']
 exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'sphinx_material'
+html_theme = 'bizstyle'
+html_static_path = ['_static']
 
 html_theme_options = {
     'logo_only': True,
