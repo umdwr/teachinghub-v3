2002965953879ad1c01872859074a3e6b271d927
Andrew Davis <adavis46@outlook.com>
2023-07-24 19:26:12 -0500

    fix?

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,6 @@ author = 'DWR'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
